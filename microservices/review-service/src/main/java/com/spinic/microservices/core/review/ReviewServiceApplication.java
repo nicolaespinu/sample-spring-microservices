d5a4ef5dab5fa3e5242f107563c366300df9a74f
Nicolae Spinu <11spinic@gmail.com>
2024-02-07 21:45:21 +0200

    UT for Review Service component

--- microservices/review-service/src/main/java/com/spinic/microservices/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/com/spinic/microservices/core/review/ReviewServiceApplication.java
@@ -2,8 +2,10 @@ package com.spinic.microservices.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.spinic")
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
--- microservices/review-service/src/test/java/com/spinic/microservices/core/review/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/com/spinic/microservices/core/review/ReviewServiceApplicationTests.java
@@ -1,13 +1,86 @@
 package com.spinic.microservices.core.review;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ReviewServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    WebTestClient webTestClient;
 
+    @Test
+    void testGetReviewsByProductId() {
+        int productId = 1;
+
+        webTestClient.get()
+                .uri("/review?productId=" + productId)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.length()").isEqualTo(3)
+                .jsonPath("$[0].productId").isEqualTo(productId);
+    }
+
+    @Test
+    void testGetReviewsMissingParameter() {
+        webTestClient.get()
+                .uri("/review")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(400)
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/review")
+                .jsonPath("$.message").isEqualTo("Required query parameter 'productId' is not present.");
+    }
+
+    @Test
+    void testGetReviewsInvalidParameter() {
+        webTestClient.get()
+                .uri("/review?productId=no-integer")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.BAD_REQUEST)
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/review")
+                .jsonPath("$.message").isEqualTo("Type mismatch.");
+
+    }
+
+    @Test
+    void testGetReviewNotFound() {
+        int productIdNotFound = 213;
+
+        webTestClient.get()
+                .uri("/review?productId=" + productIdNotFound)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.length()").isEqualTo(0);
+    }
+
+    @Test
+    void testGetReviewInvalidParameterNegativeValue() {
+        int productIdInvalid = -1;
+
+        webTestClient.get()
+                .uri("/review?productId=" + productIdInvalid)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/review")
+                .jsonPath("$.message").isEqualTo("Invalid productId: " + productIdInvalid);
+    }
 }
