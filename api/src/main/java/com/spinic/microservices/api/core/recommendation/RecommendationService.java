6fa48662308744ca3fb86f28e651e444a82f6058
Nicolae Spinu <11spinic@gmail.com>
2024-02-29 17:11:55 +0200

    Modified API component

--- api/src/main/java/com/spinic/microservices/api/core/recommendation/RecommendationService.java
+++ api/src/main/java/com/spinic/microservices/api/core/recommendation/RecommendationService.java
@@ -1,6 +1,8 @@
 package com.spinic.microservices.api.core.recommendation;
 
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -14,30 +16,10 @@ public interface RecommendationService {
     @GetMapping(
             value = "/recommendation",
             produces = "application/json")
-    List<Recommendation> getRecommendations(
+    Flux<Recommendation> getRecommendations(
             @RequestParam(value = "productId", required = true) int productId);
 
-    /**
-     * Sample usage, see below.
-     * <p>
-     * curl -X POST $HOST:$PORT/recommendation \
-     * -H "Content-Type: application/json" --data \
-     * '{"productId":123,"recommendationId":456,"author":"me","rate":5,"content":"yada, yada, yada"}'
-     *
-     * @param body A JSON representation of the new recommendation
-     * @return A JSON representation of the newly created recommendation
-     */
-    @PostMapping(
-            value = "/recommendation",
-            consumes = "application/json",
-            produces = "application/json")
-    Recommendation createRecommendation(@RequestBody Recommendation body);
+    Mono<Recommendation> createRecommendation(@RequestBody Recommendation body);
 
-    /**
-     * Sample usage: "curl -X DELETE $HOST:$PORT/recommendation?productId=1".
-     *
-     * @param productId Id of the product
-     */
-    @DeleteMapping(value = "/recommendation")
-    void deleteRecommendations(@RequestParam(value = "productId", required = true) int productId);
+    Mono<Void> deleteRecommendations(@RequestParam(value = "productId", required = true) int productId);
 }
--- api/src/main/java/com/spinic/microservices/api/core/review/ReviewService.java
+++ api/src/main/java/com/spinic/microservices/api/core/review/ReviewService.java
@@ -1,8 +1,11 @@
 package com.spinic.microservices.api.core.review;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-import java.util.List;
 
 public interface ReviewService {
     /**
@@ -14,29 +17,9 @@ public interface ReviewService {
     @GetMapping(
             value = "/review",
             produces = "application/json")
-    List<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
+    Flux<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
 
-    /**
-     * Sample usage, see below.
-     * <p>
-     * curl -X POST $HOST:$PORT/review \
-     * -H "Content-Type: application/json" --data \
-     * '{"productId":123,"reviewId":456,"author":"me","subject":"yada, yada, yada","content":"yada, yada, yada"}'
-     *
-     * @param body A JSON representation of the new review
-     * @return A JSON representation of the newly created review
-     */
-    @PostMapping(
-            value = "/review",
-            consumes = "application/json",
-            produces = "application/json")
-    Review createReview(@RequestBody Review body);
+    Mono<Review> createReview(@RequestBody Review body);
 
-    /**
-     * Sample usage: "curl -X DELETE $HOST:$PORT/review?productId=1".
-     *
-     * @param productId Id of the product
-     */
-    @DeleteMapping(value = "/review")
-    void deleteReviews(@RequestParam(value = "productId", required = true) int productId);
+    Mono<Void> deleteReviews(@RequestParam(value = "productId", required = true) int productId);
 }
